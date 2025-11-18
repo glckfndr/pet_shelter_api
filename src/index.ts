097afab72c8dc774a5f5de4770d1e3b306a3f042
glckfndr <obulan65@gmail.com>
2025-11-18 21:37:47 +0200

    feat: add params route

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 import express from "express";
 import cors from "cors";
 import type { Express, Request, Response } from "express";
-import { pets } from "./data/pets.js";
+import { pets, Pet } from "./data/pets.js";
 
 const PORT = 8000;
 const app: Express = express();
 
 app.use(cors());
 
-app.get("/", (req: Request, res: Response): void => {
+app.get("/", (req: Request, res: Response<Pet[]>): void => {
   res.json(pets);
 });
-app.use((req: Request, res: Response): void => {
+
+app.get("/:id", (req: Request<{ id: string }>, res: Response) => {
+  const pet = pets.find((p: Pet) => p.id === parseInt(req.params.id));
+  res.json(pet);
+});
+
+app.use((req: Request, res: Response<{ message: string }>): void => {
   res.status(404).json({ message: `Endpoint ${req.originalUrl} not found` });
 });
 
