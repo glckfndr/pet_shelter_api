f3a1d0c375bf73fcba2bce8c0b9c7d6ab3b3136b
glckfndr <obulan65@gmail.com>
2025-11-18 20:29:24 +0200

    feat: add handling 404 error

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,14 @@ const PORT = 8000;
 const app: Express = express();
 
 app.use(cors());
+
 app.get("/", (req: Request, res: Response): void => {
   res.json(pets);
 });
+app.use((req: Request, res: Response): void => {
+  res.status(404).json({ message: `Endpoint ${req.originalUrl} not found` });
+});
+
 app.listen(PORT, (): void => {
   console.log("Listening on port: ", PORT);
 });
