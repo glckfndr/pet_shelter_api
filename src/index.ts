506d10258c40b9e6da69f63e7d51189db8b84d4d
glckfndr <obulan65@gmail.com>
2025-11-18 20:17:56 +0200

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 import type { Express, Request, Response } from "express";
 import { pets } from "./data/pets.js";
 
 const PORT = 8000;
 const app: Express = express();
+
+app.use(cors());
 app.get("/", (req: Request, res: Response): void => {
   res.json(pets);
 });
